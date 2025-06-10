386230473084d80f8708fd9d641b56c4cc021971
lucyneitor <luz.castillo2401@alumnos.ubiobio.cl>
2025-06-10 15:51:32 -0400

    Inicio de funciones en Dao herramienta y auto

--- src/Model/AutoDAO.java
+++ src/Model/AutoDAO.java
@@ -1,4 +1,26 @@
 package Model;
+import java.util.*;
+import java.sql.*;
 
 public class AutoDAO {
+    private Connection connection;
+    public AutoDAO(){
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/taller_mecanico","root","");
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    public void crearAuto(Auto car){
+
+    }
+    public List<Auto> obtenerTodos(){
+
+    }
+    public void actualizarAuto(Auto car){
+
+    }
+    public void eliminarAuto(int id){
+
+    }
 }
--- src/Model/HerramientaDAO.java
+++ src/Model/HerramientaDAO.java
@@ -1,4 +1,29 @@
 package Model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
 public class HerramientaDAO {
+    private Connection connection;
+    public AutoDAO(){
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/taller_mecanico","root","");
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    public void crearHerramienta(Herramienta er){
+
+    }
+    public List<Auto> obtenerTodos(){
+
+    }
+    public void actualizarHerramienta(Herramienta er){
+
+    }
+    public void eliminarHerramienta(int id){
+
+    }
 }
