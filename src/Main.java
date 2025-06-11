09778cedee9554832f11f7eaf095675378fcb976
Jcaroriffo <juan.caro2401@alumnos.ubiobio.cl>
2025-06-10 22:40:39 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ public class Main {
     public static void main (String [] args){
         System.out.println("bien echo soldado");
         System.out.println("un crack totalmente");
-<<<<<<< HEAD
-        System.out.println("Nombre: ");
-=======
 
->>>>>>> tes
+        System.out.println("Nombre: ");
+
     }
 }
