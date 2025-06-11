8bf010a643085d94ea95efca22a0dea22010c128
Juan Pablo Caro Riffo <juan.caro2401@alumnos.ubiobio.cl>
2025-06-10 22:57:19 -0400

    lol

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main (String [] args){
-        System.out.println("bien echo soldado");
-        System.out.println("un crack totalmente");
 
-        System.out.println("Nombre: ");
 
     }
 }
