0894e0694463ab47a8cd6ffc8e5c827acb9abaaa
joaquinSanM <joaquin.san2301@alumnos.ubiobio.cl>
2025-06-11 22:02:21 -0400

    CCmai

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,40 @@
-public class Main {
-    public static void main (String [] args){
-        System.out.println("bien echo soldado");
-        System.out.println("un crack totalmente");
-<<<<<<< HEAD
-        System.out.println("Nombre: ");
-=======
+import Controller.ClienteController;
+import Controller.MecanicoController;
+import Controller.HerramientaController;
+import Controller.AutoController;
+import java.util.*;
 
->>>>>>> tes
+
+public class Main {
+    static Scanner teclado = new Scanner (System.in);
+    public static void main (String [] args){
+        int op;
+
+
+        do {
+            System.out.println("MENU PRINCIPAL\n1)Gestionar Auto\n2)Gestionar Clientes\n3)Gestionar Herramientas\n4)Gestionar Mecanico\n5)Salir");
+            System.out.print("Indique una opción: ");
+            op = Integer.parseInt(teclado.nextLine());
+            switch (op) {
+                case 1:
+                    new AutoController().iniciar();
+                    break;
+                case 2:
+                    new ClienteController().iniciar();
+                    break;
+                case 3:
+                    new HerramientaController().iniciar();
+                    break;
+                case 4:
+                    new MecanicoController().iniciar();
+                    break;
+                case 5:
+                    System.out.println("¡Adios!");
+                    break;
+                default:
+                    System.out.println("Opción no valida, ingrese otra.");
+
+            }
+        }while(op != 5);
     }
 }
